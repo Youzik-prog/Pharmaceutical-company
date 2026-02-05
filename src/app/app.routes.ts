7a61ee5b23e17d7b829b4707793078eed62d5397
Ivan Yurkov <youzik1999@gmail.com>
2026-02-05 18:05:14 +0300

    init router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: App },
+    {path: 'tables', component: App },
+    {path: 'process', component: App },
+    {path: 'documentation', component: App },
+    {path: '**', redirectTo: 'home'} // TODO ЗАГЛУШКА
+];
