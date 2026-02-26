431ce54f6759888fb96e0de5b8c1c464e6b687dd
Ivan Yurkov <youzik1999@gmail.com>
2026-02-26 14:24:28 +0300

    init chat component & fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { App } from './app';
 import { TableComponent } from './components/table/table.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent },
     {path: 'tables', component: TableComponent },
-    {path: 'process', component: App },
+    {path: 'chat', component: ChatComponent },
     {path: 'about', component: AboutComponent },
     {path: '**', redirectTo: 'home'} // TODO ЗАГЛУШКА
 ];
